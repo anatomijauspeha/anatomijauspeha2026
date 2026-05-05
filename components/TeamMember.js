95cd3aa690d371fb9fbf0d8a02e5925b9646e617
Anastasija <Anastasija@DESKTOP-6K6QS02>
2026-05-05 22:09:10 +0200

    debugging

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -7,8 +7,10 @@ export default function TeamMember({ member }) {
         src={member.img ? member.img : "/person.svg"}
         alt="Team member image"
         fill
+        sizes="(max-width: 768px) 100vw, 33vw"
         className="object-contain object-bottom"
       />
+      {console.log("Img:", member.img)}
       <div className="absolute bottom-3 sm:bottom-6 left-1/2 -translate-x-1/2 w-[90%] bg-[#51b957] rounded-xl sm:rounded-2xl p-2 sm:p-4 text-center shadow-lg">
         <h3 className="text-white text-sm sm:text-lg md:text-xl font-bold tracking-wide">
           {member.firstname} {member.lastname}
